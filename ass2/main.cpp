19e88d617ebc7741824e660b0927ec103a6b4181
Dror Ventura <drorventura@gmail.com>
2014-05-05 18:41:29 +0300

    ADDED - custom Texture support

--- ass2/main.cpp
+++ ass2/main.cpp
@@ -41,6 +41,65 @@ void init()
 	glLoadIdentity();
 }
 
+GLuint loadBMP_custom(const char * imagepath)
+{
+    // Data read from the header of the BMP file
+    unsigned char header[54]; // Each BMP file begins by a 54-bytes header
+    unsigned int dataPos;     // Position in the file where the actual data begins
+    unsigned int width, height;
+    unsigned int imageSize;   // = width*height*3
+
+    // Actual RGB data
+    unsigned char * data;
+
+    // Open the file
+    FILE * file = fopen(imagepath,"rb");
+    if (!file) { printf("Image could not be opened\n"); return 0; }
+
+    if ( fread(header, 1, 54, file)!=54 ){ // If not 54 bytes read : problem
+        printf("Not a correct BMP file\n");
+        return false;
+    }
+
+    if ( header[0]!='B' || header[1]!='M' ) {
+        printf("Not a correct BMP file\n");
+        return 0;
+    }
+
+    // Read ints from the byte array
+    dataPos    = *(int*)&(header[0x0A]);
+    imageSize  = *(int*)&(header[0x22]);
+    width      = *(int*)&(header[0x12]);
+    height     = *(int*)&(header[0x16]);
+
+    if (imageSize==0)    imageSize=width*height*3; // 3 : one byte for each Red, Green and Blue component
+    if (dataPos==0)      dataPos=54; // The BMP header is done that way
+
+    // Create a buffer
+    data = new unsigned char [imageSize];
+
+    // Read the actual data from the file into the buffer
+    fread(data,1,imageSize,file);
+
+    //Everything is in memory now, the file can be closed
+    fclose(file);
+
+    // Create one OpenGL texture
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+
+    // "Bind" the newly created texture : all future texture functions will modify this texture
+    glBindTexture(GL_TEXTURE_2D, textureID);
+
+    // Give the image to OpenGL
+    glTexImage2D(GL_TEXTURE_2D, 0,GL_RGB, width, height, 0, GL_BGR, GL_UNSIGNED_BYTE, data);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+    return textureID;
+}
+
 void draw_axes(void)
 {
     float ORG[3] = {0,0,0};
@@ -245,6 +304,36 @@ void mydisplay(void)
 
 }
 
+void test(void)
+{
+    glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode (GL_MODELVIEW);
+    glLoadIdentity ();
+
+    // Rotate when user changes rotate_x and rotate_y
+    glRotatef( rotate_x, 1.0, 0.0, 0.0 );
+    glRotatef( rotate_z, 0.0, 0.0, 1.0 );
+
+    GLuint texture = loadBMP_custom("brick.bmp");
+    glBindTexture (GL_TEXTURE_2D, texture);
+
+    glEnable (GL_TEXTURE_2D);
+    glBegin (GL_QUADS);
+        glTexCoord2f (0.0f,0.0f); /* lower left corner of image */
+        glVertex3f (-2.0f, -2.0f, 0.0f);
+        glTexCoord2f (1.0f, 0.0f); /* lower right corner of image */
+        glVertex3f (2.0f, -2.0f, 0.0f);
+        glTexCoord2f (1.0f, 1.0f); /* upper right corner of image */
+        glVertex3f (2.0f, 2.0f, 0.0f);
+        glTexCoord2f (0.0f, 1.0f); /* upper left corner of image */
+        glVertex3f (-2.0f, 2.0f, 0.0f);
+    glEnd ();
+
+//    glDisable (GL_TEXTURE_2D); /* disable texture mapping */
+
+    glutSwapBuffers();
+}
+
 int main(int argc, char**argv)
 {
     glutInit(&argc, argv);
@@ -260,7 +349,7 @@ int main(int argc, char**argv)
     gluLookAt(0, 0, 25, 0, 0, 0, 0, 1, 0);  //define view direction
 
     // Callback functions
-	glutDisplayFunc(mydisplay);
+	glutDisplayFunc(test);
 	glutSpecialFunc(specialKeys);
 
     //  Pass control to GLUT for events
