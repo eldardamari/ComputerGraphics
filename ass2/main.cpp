5ec00de2646a7facfb6aae92b2697d90c085f7d3
Eldar Damari <eldar.damari@gmail.com>
2014-05-07 18:14:37 +0300

    fixing order of projection and model view loading, sun is great, rotating need to be fixed

--- ass2/main.cpp
+++ ass2/main.cpp
@@ -5,13 +5,13 @@
 #include <vector>
 #include <math.h>
 #include <stdlib.h>
+#include "./Vector3f.h"
 
 #ifdef __APPLE__
     #include <GLUT/glut.h>
 #else 
     #include <GL/glut.h>
 #endif
-/*#define GL_PI 3.1415926535897932384626433832795*/
 
 #define max_hight 5.5
 using namespace std;
@@ -19,39 +19,49 @@ using namespace std;
 double rotate_y = 0;
 double rotate_x = 0;
 double rotate_z = 0;
-GLfloat zoomFactor = 1.0f;
-
+double zoom = 1;
 double MAX_HEIGHT = 5.5;
+
+GLfloat angle = 0;
 
 void init()
 {
-	float modelMatrix[16], projectionMatrix[16];
 	glClearColor(1, 1, 1, 1);
 
 	glMatrixMode(GL_PROJECTION); /* switch matrix mode */
 	glLoadIdentity();			 //load Identity matrix
 
-	gluPerspective(45, 1, 2, 25);
-	gluLookAt(20,9, 5, 0.7,0.5,1, 0, 0, 1);  //define view direction
-	
-	//glOrtho(-10.0, 10.0, -10.0, 10.0, -10.0, 10.0); //For DEBUG
 
-	/* get matrices */
-	glGetFloatv(GL_PROJECTION_MATRIX, projectionMatrix);
-	glGetFloatv(GL_MODELVIEW_MATRIX, modelMatrix);
+	gluPerspective(60, 1, 1, 100);
+	gluLookAt(0,0, 30, 0,0,0, 0, 1, 0);  //define view direction
+	
 
 	/* return to modelview mode */
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 }
 
+void drawSun(void)
+{
+
+    glPushMatrix();
+        /*glRotatef(30,15,7,8);*/
+        glRotatef(angle,1,0,0);
+
+
+        glTranslatef(15,7,8);
+        glColor3f(0.5f, 0.7f , 0.0f); 
+        glutSolidSphere(1,36,36);
+    glPopMatrix();
+}
+
 void draw_axes(void)
 {
     float ORG[3] = {0,0,0};
 
-    float XP[3] = {100,0,0}, XN[3] = {-100,0,0},
-          YP[3] = {0,100,0}, YN[3] = {0,-100,0},
-          ZP[3] = {0,0,100}, ZN[3] = {0,0,-100};
+    float XP[3] = {15,0,0}, XN[3] = {-15,0,0},
+          YP[3] = {0,15,0}, YN[3] = {0,-15,0},
+          ZP[3] = {0,0,15}, ZN[3] = {0,0,-15};
 
     glLineWidth (2.0);
     glBegin (GL_LINES);
@@ -64,16 +74,11 @@ void draw_axes(void)
     glVertex3fv (YN);
     glVertex3fv (YP);
 
-
     glColor3f (0,0,1); // Z axis is blue.
     glVertex3fv (ZN);
     glVertex3fv (ZP);
 
     glEnd();
-
-    /*glutSwapBuffers();*/
-    glFlush();
-
 }
 
 void drawCube(double x1, double y1, double z1, double x2, double y2, double z2, double h)
@@ -163,7 +168,6 @@ void drawPyramid(double x1, double y1, double z1, double x2, double y2, double z
     glEnd();
 }
 
-
 void drawHouse(double x1, double y1, double z1, double x2, double y2, double z2)
 {
     double normalizedZ1 = (z1 * MAX_HEIGHT)/255;
@@ -185,38 +189,42 @@ void drawHouse(double x1, double y1, double z1, double x2, double y2, double z2)
 //    glEnd();
 
     // WINDOW
-
-    glutSwapBuffers();
 }
 
-// ----------------------------------------------------------
-// zoomFactoospecialKeys() Callback Function
-// ----------------------------------------------------------
-void specialKeys( int key, int x, int y ) {
+void specialKeys(int key,int x,int y) {
 
-  //  Right arrow - increase rotation by 5 degree
-  if (key == GLUT_KEY_RIGHT)
-    rotate_z += 10;
-  //  Left arrow - decrease rotation by 5 degree
-  else if (key == GLUT_KEY_LEFT)
-    rotate_z -= 10;
-  else if (key == GLUT_KEY_UP)
-    rotate_x += 10;
-  else if (key == GLUT_KEY_DOWN)
-    rotate_x -= 10;
-  else if (key == GLUT_KEY_F2)
-    zoomFactor += 10;
-  else if (key == GLUT_KEY_F3)
-   zoomFactor  -= 10;
-  //  Request display update
-  glutPostRedisplay();
-}
-void setProjectionMatrix (int width, int height)
-{
-   glMatrixMode(GL_PROJECTION);
-   glLoadIdentity();
-   gluPerspective (50.0*zoomFactor, (float)width/(float)height, 1.0f, 1.0f);
-   /* ...Where 'zNear' and 'zFar' are up to you to fill in. */
+    glMatrixMode(GL_PROJECTION);
+
+    switch(key) {
+
+        case GLUT_KEY_RIGHT:
+            glRotatef( -4, 0.0, 0.0, 1.0 );
+            break;
+        case GLUT_KEY_LEFT:
+            glRotatef( 4, 0.0, 0.0, 1.0 );
+            break;
+        case GLUT_KEY_UP:
+            glRotatef( 4, 1.0, 0.0, 0.0 );
+            break;
+        case GLUT_KEY_DOWN:
+            glRotatef( -4, 1.0, 0.0, 0.0 );
+            break;
+        case GLUT_KEY_F2:
+            zoom = 1;
+            zoom += 0.1;
+            glScalef(zoom,zoom,zoom);
+            break;
+        case GLUT_KEY_F3:
+            zoom = 1;
+            zoom -= 0.1;
+            glScalef(zoom,zoom,zoom);
+            break;
+    }
+
+    glMatrixMode(GL_MODELVIEW);
+
+    //  Request display update
+    glutPostRedisplay();
 }
 
 void draw_wheel(GLfloat Cx, GLfloat Cy, GLfloat Cz)
@@ -233,7 +241,7 @@ void draw_wheel(GLfloat Cx, GLfloat Cy, GLfloat Cz)
 
 
     glBegin(GL_TRIANGLE_FAN);
-    glColor3f(1,0,0);
+    glColor3f(0.9,0.9,0.9);
     Cx += 0.2;
 
     glVertex3f(Cx,Cy,Cz);
@@ -253,7 +261,7 @@ void draw_wheel(GLfloat Cx, GLfloat Cy, GLfloat Cz)
     glFlush();
 
     glBegin(GL_TRIANGLE_FAN);
-    glColor3f(0,1,0);
+    glColor3f(0.9,0.9,0.9);
     Cx -= 0.2;
 
     glVertex3f(Cx,Cy,Cz);
@@ -285,7 +293,6 @@ void draw_wheel(GLfloat Cx, GLfloat Cy, GLfloat Cz)
 
     glFlush();
 }
-
 void drawWindShield()
 {
     glColor3f( 1.0,  0.0, 0.0 );
@@ -314,62 +321,65 @@ void drawCarBody(void)
     drawCube(0.2f,0.0f,3.5f, 1.0f, 4.0f,3.5f,2.0f);
     drawWindShield();
 }
-
-void drawCar(void)
-{
+void drawCar(void) {
     draw_wheel(0.0f ,0.0f   ,1.0f);
     draw_wheel(1.0f ,0.0f   ,1.0f);
     draw_wheel(0.0f ,5.0f  ,1.0f);
     draw_wheel(1.0f ,5.0f  ,1.0f);
 
     drawCarBody();
+}
+void rotateSun(void) {
+    angle += 0.15;
+    glutPostRedisplay();
+}
 
+void draw_plane(void) {
+
+    glBegin(GL_LINE_LOOP);
+
+        glColor3f(0,0,0);
+
+        glVertex3f(0,0,0);
+        glVertex3f(0,0,MAX_HEIGHT);
+        glVertex3f(15,7,8);
+
+    glEnd();
 
 }
 
 void mydisplay(void)
 {	
-    //  Clear screen and Z-buffer
-    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-    // Reset transformations
-    glMatrixMode(GL_PROJECTION); /* switch matrix mode */
+    // always make sure to be on MODEL_VIEW matric here!
     glLoadIdentity();
 
-    // Rotate when user changes rotate_x and rotate_y
-    glRotatef( rotate_x, 1.0, 0.0, 0.0 );
-    glRotatef( rotate_z, 0.0, 0.0, 1.0 );
-
-    glMatrixMode(GL_MODELVIEW); /* switch matrix mode */
+    //  Clear screen and Z-buffer
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 
     draw_axes();
-    drawCar();
 
+    drawCar();
     drawHouse(2.5,-2.5,7,5,-5.5,7);
+    drawSun();
+    draw_plane();
 
     glutSwapBuffers();
-
 }
 
 int main(int argc, char**argv) {
-
     glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
-
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowSize(900, 900);
 	glutCreateWindow("Assignment 2");
 
     //  Enable Z-buffer depth test
 	glEnable(GL_DEPTH_TEST);
-
-	glClearColor(1, 1, 1, 1);
-	gluPerspective(45, 1, 15, 100);
-    gluLookAt(0, 0, 30, 0, 0, 0, 0, 1, 0);  //define view direction
+    
+    init();
 
     // Callback functions
 	glutDisplayFunc(mydisplay);
-    //init();
-    //    glutReshapeFunc(changeSize);
-    //	glutIdleFunc(mydisplay);
+	glutIdleFunc(rotateSun);
     glutSpecialFunc(specialKeys);
 
     //  Pass control to GLUT for events
